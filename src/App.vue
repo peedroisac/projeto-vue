e5cfec0c0cbb275b18ac5fb877c579a5eec4bc42
peedroisac <peedroisac@gmail.com>
2024-09-12 15:47:52 -0300

    iniciando

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <NavBar />
+  <router-view />
+  <Footer />
+</template>
+
+<script>
+import NavBar from "./components/NavBar.vue";
+import Footer from "./components/Footer.vue";
+
+export default {
+  components:{
+    NavBar
+  }
+}
+</script>
+
+<style>
+</style>
